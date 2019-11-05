c86c9d4eaebbd7e5c7c24b306440b79523683a88
philshem <philip.shemella@localsearch.ch>
2019-11-05 08:34:37 +0100

    code backfilling and old files

--- README.md
+++ README.md
@@ -15,6 +15,20 @@ Doing this will save today's puzzle as a json to the folder 'data/'.
 
 Requires BeautifulSoup and requests.
 
-### to get the newest code, after you have already cloned
+---
 
-    git pull
+### using the wayback machine (archive.org) to download old puzzles
+
+Alternative usage is to provide a URL, for example, with the wayback machine:
+
+    echo https://web.archive.org/web/20190727114759/https://www.nytimes.com/puzzles/spelling-bee > url.txt
+    python3 scrape_bee.py url.txt
+
+You can also use the python library [waybackpack](https://github.com/jsvine/waybackpack) to download all URLs from the wayback machine, and then pass those urls to the scrape_bee.py script:
+
+    pip3 install waybackpack
+    waybackpack https://www.nytimes.com/puzzles/spelling-bee --list > urls.txt
+
+    python3 scrape_bee.py urls.txt
+
+(Note that many of the archived URLs have no valid puzzle, or are not available due to the subscription call-to-action.)
--- scrape_bee.py
+++ scrape_bee.py
@@ -4,12 +4,10 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import os
+import sys
 
-def main():
-	
-	# the answers are stored as a json inside the page source
-	url = 'https://www.nytimes.com/puzzles/spelling-bee'
-	
+def scrape_bee(url):
+
 	r = requests.get(url)
 	soup = BeautifulSoup(r.text,'html5lib')
 
@@ -29,5 +27,29 @@ def main():
 	print(dt)
 	
 if __name__ == '__main__':
-	main()
+	
+	# the answers are stored as a json inside the page source
+	if len(sys.argv) == 1:
+		# get today's puzzle
+		url = ['https://www.nytimes.com/puzzles/spelling-bee']
+	elif len(sys.argv) == 2:
+		try:
+			# get list of urls to scrape old puzzles from wayback machine
+			with open(sys.argv[1]) as f:
+				urls = f.readlines()
+		except:
+			print('ERROR: Not a valid file:',f)
+			exit(0)
+	else:
+		print('ERROR: Incorrect usage.')
+		print('\tScript requires either no argument, for today\'s puzzle,')
+		print('or a path to a file containing URLs. Exiting...')
+		exit(0)
+
+	for u in urls:
+		try:
+			scrape_bee(u.strip())
+			print('INFO: Valid html in',u.strip())
+		except:
+			print('WARNING: Non-matching html in',u.strip())
 
